df6e531d8fc77d0fe91c47fe745a874150c0d79b
samuelsonnega <samuelsonnega@trentu.ca>
2020-11-02 16:53:20 -0500

    New AUC code using spline method

--- AUC.R
+++ AUC.R
@@ -1,6 +1,16 @@
-cortAUC_full <- cortImputed %>% ## cortImputed = the dataset with imputed values replacing the missing values
-  group_by(Year, Litter, SquirrelID, Sex, Mum.Treat, complete, TreatmentDuring, JulianDate, age)
-%>% 
+library(tidyverse)
+library(MESS)
+cort <- read.csv("total_cort_2015-18.csv")
+cort$bleed <- as.factor(cort$bleed)
+cort$hare <- as.factor(cort$hare)
+cort$year <- as.factor(cort$year)
+cort$cort <- as.numeric(cort$cort)
+
+cortAUC_full <- cort %>% ## cortImputed = the dataset with imputed values replacing the missing values
+  group_by(year, season, hare) %>% 
   ## grouped by all these variables so they would stay in the dataset after summarizing
-  summarize(AUC_imputed = auc(sample,Cort3, from = 2, to = 4, type = "spline", na.rm = TRUE)) 
-## auc calculates the area under the curve using 'sample' as the key (base = 1, dex = 2, 30 = 3, 60 = 4) and 'Cort3' as the variable
+  summarize(AUC = MESS::auc(bleed,cort, from = 2, to = 5, type = "spline", na.rm = TRUE)) 
+## auc calculates the area under the curve using 'sample' as the key 
+##(base = 1, dex = 2, 30 = 3, 60 = 4) and 'Cort3' as the variable
+
+write.csv(cortAUC_full, "cortAUC.csv")
