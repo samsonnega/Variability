5620fafe6666474c29593712b614bf5f56daf465
samuelsonnega <samuelsonnega@trentu.ca>
2020-11-02 22:02:23 -0500

    increase warm up and add means by phase

--- cond_bayes_models.R
+++ cond_bayes_models.R
@@ -28,18 +28,19 @@ str(cond)
 m1_brm <- brm(scale_bc ~ month +
                 (1 | hare) + (1 | year/month),
               data = cond,
-              warmup = 500,
+              warmup = 1000,
               iter = 3000,
               thin=2,
               chains = 2,
               inits = "random",
               cores = parallel:::detectCores(),
               seed = 12345,
-              control = list(adapt_delta = 0.85)) ##this helps convergence
+              control = list(adapt_delta = 0.99)) ##this helps convergence
 m1_brm <- add_criterion(m1_brm, "waic")
 
 save(m1_brm,file = "m1_brm_cond.rds")
 
+load("m1_brm_cond.rds")
 summary(m1_brm)
 plot(m1_brm)
 pairs(m1_brm)
@@ -66,6 +67,88 @@ RRes <- var.res / (var.animal_id + var.year.month + var.year + var.res)
 mean(RRes)
 
 # Similar, and as explained in the frequentist section, we can calculate $CV_i$ as:
-CVi <- sqrt(var.animal_id) / mean(cond$scale_bc)
+CVi <- sqrt(var.animal_id) / mean(cond$bc)
 CV.cond <- mean(CVi);HPDinterval(as.mcmc(CVi),0.95)
-CV.cond*100
+
+
+mean(var.animal_id)
+
+
+### examine levels of variability
+# Vh & Vw ??? by environments
+model.brms=bf(bc ~ phase + month + (0+phase||hare), sigma ~ 0+phase)
+fit_model.brms <- brm(model.brms, data = cond,
+                      cores    = parallel:::detectCores(),
+                      refresh = 0)
+summary(fit_model.brms) 
+plot(fit_model.brms)
+
+#make sure priors work
+prior_summary(fit_model.brms)
+
+colnames(posterior_samples(fit_model.brms))[1:10]
+
+#extract variance components 
+var.brms <- posterior_samples(fit_model.brms, 
+                              c("sd_hare__phasepeak", # among unit sd E1
+                                "sd_hare__phasedecline", # among unit sd E2
+                                "sigma_phasepeak", # within unit sd E1, log scale
+                                "sigma_phasedecline")) # within unit sd E2, log scale
+
+colnames(var.brms)=c("Vh.P", "Vh.D", "Vw.P", "Vw.D")
+var.brms=as.data.frame(var.brms)
+head(var.brms)
+#We need to square these values to express theme as variances and take the 
+#exponent for the residual standard deviation:
+
+var.brms$Vh.P=(var.brms$Vh.P)^2
+var.brms$Vh.D=(var.brms$Vh.D)^2
+var.brms$Vw.P=exp(var.brms$Vw.P)^2
+var.brms$Vw.D=exp(var.brms$Vw.D)^2
+
+var.brms$tau.P=var.brms$Vh.P/(var.brms$Vh.P+var.brms$Vw.P)
+var.brms$tau.D=var.brms$Vh.D/(var.brms$Vh.D+var.brms$Vw.D)
+
+var.brms$delta.Vh=var.brms$Vh.D-var.brms$Vh.P
+var.brms$delta.Vw=var.brms$Vw.D-var.brms$Vw.P
+var.brms$delta.tau=var.brms$tau.D-var.brms$tau.P
+
+var.brms=stack(var.brms)
+var.brms.table=var.brms %>% group_by(ind) %>%
+  summarise(mode=posterior.mode(as.mcmc(values)),
+            low.ci=HPDinterval(as.mcmc(values))[1],
+            up.ci=HPDinterval(as.mcmc(values))[2]) %>% data.frame()
+var.brms.table[,-1]=round(var.brms.table[,-1],2)
+var.brms.table
+
+#plot the posterior mode differences
+cond.bayes <- var.brms %>% filter(.,ind=="delta.Vh" | 
+                                     ind=="delta.Vw" | 
+                                     ind=="delta.tau") %>% 
+  ggplot(., aes(x=values, y=ind, fill=ind)) +
+  geom_halfeyeh() + geom_vline(xintercept=0,linetype="dashed") +
+  ylab("") + xlab("Standardized Posterior Mode") +
+  scale_fill_wsj() +
+  theme_base() + theme(legend.position = "none")
+
+cond.bayes
+#plot just the phase difference in metrics
+fecal.bayes.2 <- var.brms %>% filter(.,ind=="Vh.P" | 
+                                       ind=="Vh.D") %>% 
+  ggplot(., aes(x=values, y=ind, fill=ind)) +
+  geom_halfeyeh() + geom_vline(xintercept=0,linetype="dashed") +
+  ylab("") + xlab("standardized posterior mode") +
+  scale_fill_wsj() +
+  theme_base() + theme(legend.position = "none")
+
+fecal.bayes.2
+
+fecal.bayes.3 <- var.brms %>% filter(.,ind=="Vw.P" | 
+                                       ind=="Vw.D") %>% 
+  ggplot(., aes(x=values, y=ind, fill=ind)) +
+  geom_halfeyeh() + geom_vline(xintercept=0,linetype="dashed") +
+  ylab("") + xlab("standardized posterior mode") +
+  scale_fill_wsj() +
+  theme_base() + theme(legend.position = "none")
+
+fecal.bayes.3
