2e970eded190e0aae4c6b44a1f7edf82ad60266a
samuelsonnega <samuelsonnega@trentu.ca>
2020-11-05 16:45:38 -0500

    convert foraging to min

--- behavior tidy.R
+++ behavior tidy.R
@@ -4,10 +4,12 @@ behav$Date<-as.POSIXct(strptime(as.character(behav$Date),"%d/%m/%Y"))
 behav2 <- behav %>% 
   select(id = 'Hare',
          date = 'Date',
-         forage = 'Foraging') %>% 
+         forage = 'Foraging',
+         hour = 'Hour') %>% 
   mutate(month = lubridate::month(date),
          year = lubridate::year(date),
          day = lubridate::day(date),
+         foragemin = (forage/60),
          phase = case_when(year==2015 ~ "peak",
                              year==2016 ~ "peak",
                              year==2017 ~ "decline",
