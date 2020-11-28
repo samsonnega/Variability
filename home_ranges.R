7c164a94e267ba8eee6a3a1d1b07654f67b76257
samuelsonnega <samuelsonnega@trentu.ca>
2020-11-28 00:02:20 -0500

    plot all data 2015-20

--- home_ranges.R
+++ home_ranges.R
@@ -1,6 +1,6 @@
 ###Using package amt -------
 #load in the data
-data<-read.csv("gps2015-18.csv")
+data<-read.csv("GPS_2015_2020.csv")
 #load packages
 library(lubridate)
 library(tidyverse)
@@ -10,17 +10,17 @@ library(amt)
 library(sf)
 
 #first fix date
-data$date2<-as.POSIXct(strptime(as.character(data$date2),"%d/%m/%Y %H:%M"))
+data$date<-as.POSIXct(strptime(as.character(data$date),"%d/%m/%Y %H:%M"))
 
 #Take what matters and add year
 data2 <- data %>%
   filter(!is.na('y'))  %>%
-  mutate(year = lubridate::year(date2),
-         month = lubridate::month(date2),
-         yday = lubridate::yday(date2)) %>% 
+  mutate(year = lubridate::year(date),
+         month = lubridate::month(date),
+         yday = lubridate::yday(date)) %>% 
   select(x_ = 'x', y_ = 'y',
-         t_ = 'date2', id = 'id',
-         p = 'phase', yr = 'year',
+         t_ = 'date', id = 'id',
+          yr = 'year',
          m = 'month', yd = 'yday')
 
 #make sure the dataset is complete
@@ -37,7 +37,7 @@ data2 <- data2 %>% filter(!ind2)
 
 #...build the track ----
 trk <- data2 %>%
-  make_track(x_, y_, t_, id = id, yr = yr, p = p, crs = CRS("+init=epsg:32607"))
+  make_track(x_, y_, t_, id = id, yr = yr, crs = CRS("+init=epsg:32607"))
 
 has_crs(trk)
 get_crs(trk)
@@ -95,7 +95,7 @@ hr1 <- dat1 %>%
 saveRDS(hr1, "hr1.RDS")
 
 hr2 <- hr1 %>% select(-data) %>%
-  pivot_longer(hr_mcp:hr_akde_ou, names_to = "estimator",
+  pivot_longer(hr_kde, names_to = "estimator",
                values_to = "hr")
 
 hr2.area <- hr2 %>%
@@ -110,33 +110,38 @@ head(hr2.area, 2)
 hr3.area <- hr2.area %>% 
   select(id = 'id',
          year = 'yr',
-         phase = 'p',
          estimator = 'estimator',
          level = 'level',
          area = 'area') %>% 
   mutate(area = area / 1e4)
 
-write.csv(hr3.area, "hr3.csv")
-
+write.csv(hr3.area, "hr2.area1.csv")
+#fix the outliers
+hr2.area2 <- read.csv("hr2.area1.csv")
 #plot the means
 # arrange into more tidy format
 hr2.area1 <- hr2.area %>%
   mutate(area = area / 1e4)
-hr2.area1$est_lab <- factor(hr2.area1$estimator,
-                            levels = c(
-                              "hr_mcp", "hr_kde", "hr_locoh", "hr_akde_iid", "hr_akde_ou", "hr_akde_auto",
-                              "hr_akde_iid_with_error", "hr_akde_ou_with_error", "hr_akde_auto_with_error"),
-                            labels = c("MCP", "KDE", "LoCoH", "aKDE (iid)", "aKDE (ou)", "aKDE (select)",
-                                       "aKDE (iid, e)", "aKDE (ou, e)", "aKDE (select, e)"))
-ci <- hr2.area1 %>% group_by(est_lab, yr) %>%
+hr2.area2$year <- factor(hr2.area2$year,
+                            levels = c("2015","2016","2017","2018","2019","2020"),
+                            labels = c("2015","2016","2017","2018","2019","2020"))
+ci <- hr2.area2 %>% group_by(year) %>%
   summarise(m = mean(area), se = sd(area) / sqrt(n()),
-            me = qt(0.975, n() - 1) * se, lci = m - me, uci = m + me)
+            me = qt(0.975, n() - 1) * se, lci = m - me, uci = m + me,
+            samp = n())
 
-p1 <- hr2.area1 %>% ggplot(aes(est_lab, area, col = yr)) +
-  geom_pointrange(aes(x = est_lab, y = m, ymin = lci, ymax = uci, col = yr), data = ci, inherit.aes = FALSE,
+p1 <- hr2.area2 %>% ggplot(aes(year, area)) +
+  geom_errorbar(width = .1, aes(x = year, y = m, ymin = lci, ymax = uci), data = ci, inherit.aes = FALSE,
                   position = position_dodge2(width = 0.5)) +
-  theme_light() +
+  geom_point(data = ci, aes(x = year, y = m, size = samp), shape = 21, fill = "black") +
+  theme_classic(base_size = 25) +
   theme(axis.title.x = element_blank()) +
-  labs(y = expression(paste("HRS [", ha, "]")))
+  labs(y = expression(paste("KDE [", ha, "]")))
 
-p1
+p1 + theme(legend.position = "none")
+p1 + geom_text(aes(label = samp, y = 18) ,data = ci)
+p1 + theme()
+
+ggsave(p1, 
+       file = "Home_Range.jpg", 
+       width = 12, height = 6, unit = "in", dpi = 300)
